28bdb2bf7bc8d664f78fdb4897db4417fc94dfa2
Michael Thompson <michael.220488@gmail.com>
2020-01-22 12:44:35 -0800

    setting up files

---
+++ fizzy.js
@@ -0,0 +1,15 @@
+//declare function in ES6 with input of an integer
+const fizzy = (n) => {
+
+  //
+  
+  //
+  
+  //
+  
+  /*return an array of numbers from 0 to n with:
+  - multiples of 3 replaced by 'fizz'
+  - multiples of 5 replaced by 'buzz'
+  - multiples of both replaced by 'fizzbuzz'
+  */
+  }
---
+++ fizzy.test.js
@@ -0,0 +1,8 @@
+//test whether or not fizzy is a function
+
+//test whether or not the input is an integer
+
+//test whether or not the output is an array
+
+//
+

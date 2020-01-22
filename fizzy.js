65002b8464600796ea7ead8c14e08f9990700c36
Michael Thompson <michael.220488@gmail.com>
2020-01-22 14:06:53 -0800

    fixed error in test

--- fizzy.js
+++ fizzy.js
@@ -1,14 +1,11 @@
 //declare function in ES6 with input of an integer
-const fizzy = (n) => {
-
+exports.fizzy = (n) => {
   //create new Array that fills in numbers from 1 to n 
-    return Array.from({length: n}, (v, k)=> k+1)
-    .map(e =>  e % 3 === 0 && e % 5 === 0 ? e = 'fizzbuzz' : e % 5 === 0 ? e = 'buzz' : e % 3 === 0 ? e = 'fizz' : e = e)
-
-  
+   return Array.from({length: n}, (v, i)=> i+1).map(e =>  e % 3 === 0 && e % 5 === 0 ? e = 'fizzbuzz' : e % 5 === 0 ? e = 'buzz' : e % 3 === 0 ? e = 'fizz' : e = e)
   /*return an array of numbers from 1 to n with:
   - multiples of 3 replaced by 'fizz'
   - multiples of 5 replaced by 'buzz'
   - multiples of both replaced by 'fizzbuzz'
   */
   }
+ 
--- fizzy.test.js
+++ fizzy.test.js
@@ -1,6 +1,9 @@
 const { expect } = require('chai')
-
+const { fizzy } = require('./fizzy.js')
 //test whether or not fizzy is a function
+//test whether or not the input is an integer
+//test whether or not the output is an array
+//test whether the array deep equals a correct array
 describe('Fizzy', function(){
   it('should be a function', function(){
     expect(fizzy).to.be.a('function')
@@ -9,15 +12,11 @@ describe('Fizzy', function(){
     expect(fizzy()).to.be.a('array')
   })
   it('should return an array where multiples of 3 are replaced by fizz, 5 are replaced by buzz', function(){
-    expect(fizzy(3).to.deep.equal([1, 2, 'fizz', 4, 'buzz']))
+    expect(fizzy(5)).to.deep.equal([1, 2, 'fizz', 4, 'buzz'])
   })
   it('should return an array where multiples of both 3 and 5 are replaced by fizzbuzz', function(){
-    expect(fizzy(15).to.deep.equal([1, 2, 'fizz', 4, 'buzz', 6, 7, 8, 'fizz', 'buzz', 11, 'fizz', 13, 14, 'fizzbuss']))
+    expect(fizzy(15)).to.deep.equal([1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz', 11, 'fizz', 13, 14, 'fizzbuzz'])
   })
 })
-//test whether or not the input is an integer
 
-//test whether or not the output is an array
-
-//
 

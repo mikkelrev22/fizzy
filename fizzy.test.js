ab9791bf437b217b7a1e31ca51b0a9a255734794
Michael Thompson <michael.220488@gmail.com>
2020-01-22 13:12:42 -0800

    BDD tests written using mocha chai

--- fizzy.test.js
+++ fizzy.test.js
@@ -1,5 +1,20 @@
+const { expect, should } = require('chai')
+const { assert } = require('mocha')
 //test whether or not fizzy is a function
-
+describe('Fizzy', function(){
+  it('should be a function', function(){
+    expect(fizzy).to.be.a('function')
+  })
+  it('should return an array', function(){
+    expect(fizzy()).to.be.a('array')
+  })
+  it('should return an array where multiples of 3 are replaced by fizz, 5 are replaced by buzz', function(){
+    expect(fizzy(3).to.deep.equal([1, 2, 'fizz', 4, 'buzz']))
+  })
+  it('should return an array where multiples of both 3 and 5 are replaced by fizzbuzz', function(){
+    expect(fizzy(15).to.deep.equal([1, 2, 'fizz', 4, 'buzz', 6, 7, 8, 'fizz', 'buzz', 11, 'fizz', 13, 14, 'fizzbuss']))
+  })
+})
 //test whether or not the input is an integer
 
 //test whether or not the output is an array

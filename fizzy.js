8fa754798ef90ae523378257d6c86f749cd3d033
Michael Thompson <michael.220488@gmail.com>
2020-01-23 19:38:42 -0800

    working on get request

--- fizzy.js
+++ fizzy.js
@@ -2,7 +2,7 @@
 exports.fizzy = (n) => {
   
   //create new Array that fills in numbers from 1 to n 
-   return resArr = Array.from({length: n}, (v, i)=> i+1).map(e =>  e % 3 === 0 && e % 5 === 0 ? e = 'fizzbuzz' : e % 5 === 0 ? e = 'buzz' : e % 3 === 0 ? e = 'fizz' : e = e)
+   return Array.from({length: n}, (v, i)=> i+1).map(e =>  e % 3 === 0 && e % 5 === 0 ? e = 'fizzbuzz' : e % 5 === 0 ? e = 'buzz' : e % 3 === 0 ? e = 'fizz' : e = e)
    /*return an array of numbers from 1 to n with:
   - multiples of 3 replaced by 'fizz'
   - multiples of 5 replaced by 'buzz'
--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require('express')
 const app = express()
-const PORT =7001
+const PORT = 7001
+const { fizzy } = require('./fizzy.js')
+const morgan =  require('morgan')
 
+app.use(morgan('dev'))
 
+app.get('/:id', (req, res)=>{
+  // console.log(fizzy(req.params.id))
+  fizzy(req.params.id), (err, data) => {
+    if (err) return next(err)
+    console.log(data)
+  }
+})
+
+app.use((req, res, next) => {
+  res.sendStatus(404)
+})
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.sendStatus(500).send(err.stack)
+})
 
 app.listen(PORT, ()=>{
   console.log(`Server is now listening on Port ${PORT}`)

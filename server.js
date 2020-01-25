63973a88ade9a6af13d7363393ca89f9ee50b4fb
Michael Thompson <michael.220488@gmail.com>
2020-01-24 16:25:06 -0800

    need to add error handling

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ const { fizzy } = require('./fizzy.js')
 const morgan =  require('morgan')
 
 app.use(morgan('dev'))
-
-app.get('/:id', (req, res)=>{
-  // console.log(fizzy(req.params.id))
-  fizzy(req.params.id), (err, data) => {
-    if (err) return next(err)
-    console.log(data)
-  }
+app.get('/:id', (req, res) => {
+  res.send(fizzy(req.params.id))
 })
+
+// app.get('/:id', (req, res)=>{
+//   console.log(req.params.id)
+//   fizzy(req.params.id), (err, data) => {
+//     if (err) return next(err)
+//     res.setStatus(200).send(data)
+//   }
+// })
 
 app.use((req, res, next) => {
   res.sendStatus(404)
